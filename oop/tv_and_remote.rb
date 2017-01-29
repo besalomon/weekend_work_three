7242d0e52b441e66587dcf03c7cb882f89e1412d
Salomon Beza <beza.salomon@gmail.com>
2017-01-29 16:23:29 +0200

    finished the last exercise under oop

--- oop/tv_and_remote.rb
+++ oop/tv_and_remote.rb
@@ -10,3 +10,81 @@
 # increment_volume (this will increase the tv's volume by 1)
 # decrement_volume (this will decrease the tv's volume by 1)
 # set_channel (this will change the tv's channel to whatever integer is passed to this method)
+
+
+
+class Remote
+  def initialize(tv_number)
+    @tv = tv_number[:tv]
+  end
+  def toggle_power
+    if power == "ON"
+      @power = "OFF"
+    else
+     @power = "ON"
+    end
+  end
+
+  def increment_volume
+    @volume += 1
+  end
+
+  def decrement_volume
+    @volume -= 1
+  end
+
+  def set_channel(var)
+    @channel = var
+  end
+
+end
+
+class Tv < Remote
+attr_accessor :power, :volume, :channel
+
+def initialize(tv_info) 
+    @power = tv_info[:power]
+    @volume = tv_info[:volume]
+    @channel = tv_info[:channel]
+end
+
+end
+
+tv = Tv.new({power:"OFF", volume:25, channel:2})
+remote = Remote.new(tv:[tv])
+
+action1 = tv.toggle_power
+
+if action1 == "ON"
+  puts "PASS: The TV is ON"
+else
+  puts "F"
+end
+
+action2 = tv.set_channel(5)
+
+if action2 == 5
+  puts "PASS: Switched to channel 5"
+else
+  puts "F"
+end
+
+action3 = 0
+
+5.times do
+action3 = tv.increment_volume
+end
+
+if action3 == 30
+  puts "PASS: Volume set to 30"
+else
+  puts "F"
+end
+
+action4 = tv.toggle_power
+
+if action4 == "OFF"
+  puts "PASS: You turned the tv off"
+else
+  puts "F"
+end
